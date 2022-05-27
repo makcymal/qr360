a26983d98923d1476529634daf8ddd5eecf4b08d
the-makcym <the.makcym@yandex.ru>
2022-05-27 20:42:24 +1000

    router + authorization page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,52 +1,14 @@
 <template>
-  <div class="bg top whit-bg"></div>
-  <div class="bg bot lgry-bg"></div>
-
-  <div class="cols-wrapper">
-  	<div class="col">
-  		<div class="cont big blue-bg">
-  			<h2 class="text sml whit-fn">
-	  			Меняйте
-	  			<br>содержимое
-	  			<br>QR кода,
-	  			<br>даже если уже
-	  			<br>отправили
-	  			<br>его
-	  			<br>всем
-	  			<br>друзьям
-  			</h2>
-  		</div>
-  		<div class="cont sml">
-  			<img src="@/img/qr.svg" class="qr">
-  		</div>
-  	</div>
-  	<div class="col center">
-  		<div class="cont big dgry-bg">
-  			<svg xmlns="http://www.w3.org/2000/svg" fill="#DFE4EA" class="alt" viewBox="0 0 16 16">
-				  <path d="M1 13.5a.5.5 0 0 0 .5.5h3.797a.5.5 0 0 0 .439-.26L11 3h3.5a.5.5 0 0 0 0-1h-3.797a.5.5 0 0 0-.439.26L5 13H1.5a.5.5 0 0 0-.5.5zm10 0a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 0-1h-3a.5.5 0 0 0-.5.5z"/>
-				</svg>
-				<h1 class="text lrg whit-fn">QR360</h1>
-  		</div>
-  	</div>
-  	<div class="col">
-  		<div class="cont sml btm">
-  			<button class="btn text sml dgry-bg whit-fn">
-  				Авторизация
-  			</button>
-  		</div>
-  		<div class="cont big orng-bg">
-  			<h3 class="text mdm whit-fn">Как?</h3>
-  			<h3 class="text xs pad-top whit-fn">
-	  			Созданный у нас QR будет вести на промежуточную страницу, которая направит Вашего клиента туда, куда укажете Вы
-  			</h3>
-  		</div>
-  	</div>
-  </div>
+	<div>
+  	<router-view></router-view>
+	</div>
 </template>
 
 
 <script>
-  
+export default {
+
+}
 </script>
 
 
@@ -58,68 +20,21 @@ body {
 	padding: 0;
 }
 
+button {
+	border: 0;
+}
+
+a {
+	text-decoration: none;
+}
+
 .bg {
-	position: absolute;
 	width: 100%;
-	left: 0;
-}
-
-.bg.top {
-	height: 30vh;
-	top: 0;
-}
-
-.bg.bot {
-	height: 70vh;
-	bottom: 0;
-}
-
-.cols-wrapper {
-	position: absolute;
-	width: 80%;
-	height: 80vh;
-	top: 10%;
-	left: 10%;
-	display: flex;
-	flex-direction: row;
-	justify-content: space-evenly;
-}
-
-.col {
-	width: 25%;
-	height: 100%;
-	display: flex;
-	flex-direction: column;
-	justify-content: space-between;
-	user-select: none;
-}
-
-.col.center {
-	justify-content: center;
-}
-
-.cont {
-	width: 100%;
+	height: 100vh;
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
 	align-items: center;
-}
-
-.cont.big {
-	height: 65%;
-}
-
-.cont.sml {
-	height: 25%;
-}
-
-.qr {
-	height: 100%;
-}
-
-.alt {
-	width: 40%;
 }
 
 .text {
@@ -131,7 +46,7 @@ body {
 }
 
 .lrg {
-	font-size: min(3.5em, 9vh, 5vw);
+	font-size: min(3em, 8vh, 4vw);
 }
 
 .mdm {
@@ -146,21 +61,8 @@ body {
 	font-size: min(1.1em, 2.5vh, 1.5vw);
 }
 
-.pad-top {
-	padding-top: min(1em, 1vh);
-}
-
-.btn {
-	font-weight: 600;
-	letter-spacing: 0;
-	padding: max(0.7em, 0.5vh, 0.2vw) max(1em, 0.8vh, 0.5vw);
-	border: 0;
-	border-radius: 5px;
-	transition: all 0.3s ease-out;
-}
-
-.btn:hover {
-	background-color: #FC5819;
+.invisible {
+	opacity: 0;
 }
 
 </style>
