9082bbc0dd654cb1f319684a6680cd8e4863da1f
the-makcym <the.makcym@yandex.ru>
2022-05-28 14:53:49 +1000

    router bug fix

--- backend/qr/models.py
+++ backend/qr/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+	phone 		  = models.CharField(max_length=12)
+	password_hash = models.CharField(max_length=64, default="")
+
+	def __str__(self):
+		return self.phone
+
+
+class Session(models.Model):
+	user 		 = models.ForeignKey(User, on_delete=models.CASCADE)
+	session_hash = models.CharField(max_length=64)
+		
+	def __str__(self):
+		return str(user)
--- frontend/src/router/router.ts
+++ frontend/src/router/router.ts
@@ -1,24 +1,19 @@
-import HomePage from "@/pages/HomePage.vue"
-import AuthPage from "@/pages/AuthPage.vue"
-import { createRouter, createWebHistory } from "vue-router"
-
+import HomePage from "@/pages/HomePage.vue";
+import AuthPage from "@/pages/AuthPage.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
-	{
-		path: "/",
-		component: HomePage,
-	},
-	{
-		path: "/auth",
-		component: AuthPage,
-	}
-]
+  {
+    path: "/",
+    component: HomePage,
+  },
+  {
+    path: "/auth",
+    component: AuthPage,
+  },
+];
 
-
-const router = createRouter({
-	routes, 
-	history: createWebHistory(process.env.BASE_URL),
-})
-
-
-export default router;
+export default createRouter({
+  routes,
+  history: createWebHistory(process.env.BASE_URL),
+});
