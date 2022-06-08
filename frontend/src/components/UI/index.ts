ec7a5a351b1b87706cc047837bf9215dd910b217
the-makcym <the.makcym@yandex.ru>
2022-06-08 16:28:29 +1000

    setup

--- frontend/src/components/UI/index.ts
+++
@@ -1,7 +0,0 @@
-import AuthButton from "@/components/UI/AuthButton.vue";
-import EasyInput from "@/components/UI/EasyInput.vue";
-import EasyButton from "@/components/UI/EasyButton.vue";
-import EasyIcon from "@/components/UI/EasyIcon.vue";
-import EasyMsg from "@/components/UI/EasyMsg.vue";
-
-export default [AuthButton, EasyInput, EasyButton, EasyIcon, EasyMsg];
