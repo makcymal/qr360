c307b2a47e32e0f4639500d4323d5d2a6cc0ba87
the-makcym <the.makcym@yandex.ru>
2022-06-21 14:07:24 +1000

    all deleted

--- front/src/components/UI/index.ts
+++
@@ -1,6 +0,0 @@
-import EasyInput from "@/components/UI/EasyInput.vue";
-import EasyButton from "@/components/UI/EasyButton.vue";
-import EasyIcon from "@/components/UI/EasyIcon.vue";
-import EasyMsg from "@/components/UI/EasyMsg.vue";
-
-export default [EasyInput, EasyButton, EasyIcon, EasyMsg];
